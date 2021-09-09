7757b88f0090e1adbeba88715741fa4bdbb82ea0
raphyabak <raphyabak@gmail.com>
2021-09-09 18:35:50 +0100

    Added Seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $items = [
+
+            ['id' => 1, 'name' => 'Admin',],
+            ['id' => 2, 'name' => 'Advisor',],
+            ['id' => 3, 'name' => 'student',],
+
+        ];
+
+        foreach ($items as $item) {
+            Role::create($item);
+        }
+    }
+}
