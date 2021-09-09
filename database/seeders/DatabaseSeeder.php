d486e94f6c875142ff6fda377c983ee559279a97
raphyabak <raphyabak@gmail.com>
2021-09-09 18:42:59 +0100

    Updated Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +16,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(RoleSeeder::class);
+      $user =  \App\Models\User::create([
+            'name' => 'Raphael Abayomi',
+            'email' => 'raphyabak@gmail.com',
+            'password' => Hash::make(abakraphy),
+        ]);
+
+        $user->assignRole($this->role);
+
     }
 }
